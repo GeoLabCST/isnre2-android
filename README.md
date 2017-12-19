543beb6862c9882438b1f08f49961de49b70c6aa
sakda homhuan <sakda.homhuan@gmail.com>
2017-12-19 22:27:58 +0700

    add map

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # isnre2-android
-android app
+
+#start app
+$ npm install
+
+# install gear
+$ npm install leaflet --save
+
+$ ionic cordova plugin add cordova-plugin-geolocation 
+$ npm install @ionic-native/geolocation --save 
+
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import L from 'leaflet';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,80 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public map: L.map;
+  public marker: L.marker; 
+  public items: string[]; 
 
+  constructor(
+    public navCtrl: NavController
+  ) {
+    this.initializeItems();
+  }
+
+  ionViewDidLoad(){
+    this.loadMap();  
+  }  
+
+  loadMap(){  
+    this.map = L.map('map',{
+      center: [13.00, 101.50],
+      zoom: 5,
+      zoomControl: false
+    })
+
+    let osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attributions: 'OSM'
+    });
+
+    let mapbox = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access' +
+        '_token=pk.eyJ1IjoicGF0cmlja3IiLCJhIjoiY2l2aW9lcXlvMDFqdTJvbGI2eXUwc2VjYSJ9.trTzs' +
+        'dDXD2lMJpTfCVsVuA').addTo(this.map);
+
+    let baseLayers = {
+      "Mapbox": mapbox,
+      "OpenStreetMap": osm
+    };
+
+    let overlays = {
+      //"hcenter": hcenter,
+      //"dengue": dengue
+    };
+    //L.control.layers(baseLayers, overlays).addTo(this.map);  
+    //this.showLocation();
+  }
+
+  initializeItems() {
+      this.items = [
+      'บึงกาฬ',
+      'บุรีรัมย์',
+      'ขอนแก่น',
+      'กระบี่',
+      'ปทุมธานี',
+      'พัทลุง',
+      'พะเยา',
+      'พิษณุโลก',
+      'ปราจีนบุรี',
+      'ประจวบคิรีขันธ์',
+      'ราชบุรี',
+      'ระยอง',
+      'สระแก้ว',
+      'สระบุรี',
+    ]
+  }
+
+  getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
